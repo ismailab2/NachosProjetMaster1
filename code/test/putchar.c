3d128bfe6e3f91220532c7992e4906fe938aa934
Abdeldjabar Ismail Abdraman <aismailabdra@emi.u-bordeaux.fr>
2025-10-01 00:08:00 +0200

    correction général

--- code/test/putchar.c
+++ code/test/putchar.c
@@ -1,3 +1,6 @@
+
+#ifdef CHANGED;
+
 #include "syscall.h"
 
 void print(char c, int n)
@@ -17,3 +20,5 @@ void print(char c, int n)
         print('a',4);
         Halt();
     }
+
+    #endif //   CHANGED
